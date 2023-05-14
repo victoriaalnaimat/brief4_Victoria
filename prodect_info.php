ef5982c4685b5f2a27ec7d0bb9a08677592235ef
victoriaalnaimat <victoriaabadi24@gmail.com>
2023-05-14 12:20:40 +0300

    y87

--- prodect_info.php
+++ prodect_info.php
@@ -6,19 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Product info</title>
 </head>
 
 <body>
     <?php
-    //connect to the database
-    $conn = mysqli_connect("localhost", "root", "", "brief4");
+    @include 'config.php';
 
-    //check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    session_start();
+
+    $user_id = $_SESSION['user_id'];
+
+    /*if (!isset($user_id)) {
+            header('location:login.php');
+        };*/
 
     //retrieve the product ID from the URL parameter
     if (isset($_GET['id'])) {
@@ -31,6 +34,43 @@
     $sql = "SELECT * FROM products WHERE id = '$product_id'";
     $result = mysqli_query($conn, $sql);
 
+    // add product to cart when "add to cart" button is clicked
+    if (isset($_POST['add_to_cart'])) {
+
+        // create an array to hold product data
+        $product_data = array(
+            'product_id' => $_POST['product_id'],
+            'product_name' => $_POST['product_name'],
+            'product_price' => $_POST['product_price'],
+            'product_image' => $_POST['product_image'],
+            'product_quantity' => $_POST['product_quantity']
+        );
+
+        // if cart is empty, create a new session variable to hold cart items
+        if (!isset($_SESSION['cart_items'])) {
+            $_SESSION['cart_items'] = array();
+        }
+
+        // check if product is already in cart
+        $product_exists = false;
+        foreach ($_SESSION['cart_items'] as $key => $value) {
+            if ($value['product_id'] == $product_data['product_id']) {
+                // update product quantity
+                $_SESSION['cart_items'][$key]['product_quantity'] += $product_data['product_quantity'];
+                $product_exists = true;
+                break;
+            }
+        }
+
+        // if product is not in cart, add it to cart
+        if (!$product_exists) {
+            $_SESSION['cart_items'][] = $product_data;
+        }
+
+        // display success message
+        echo 'product added to cart';
+    }
+
     //display the product details
     if (mysqli_num_rows($result) > 0) {
         $row1 = mysqli_fetch_assoc($result);
@@ -38,7 +78,8 @@
         <div class="container col-xxl-8 px-4 py-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div class="col-10 col-sm-8 col-lg-6">
-                    <img src="<?= $row1['img_url'] ?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                    <img src="<?= $row1['img_url'] ?>" class="d-block mx-lg-auto img-fluid"
+                        alt="Bootstrap Themes" width="700" height="500" loading="lazy">
                 </div>
                 <div class="col-lg-6">
                     <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
@@ -51,36 +92,29 @@
                         <?= $row1['price'] ?>
                     </p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <a href="cart_items.php?id=<?= $items['id'] ?>"
-                        
-                        <?php
-                        if (isset($_POST['add_to_cart'])) {
-                            $product_id=$_POST['product_id'];
-                            $product_name=$_POST['name'];
-                            $product_price=$_POST['price'];
-                            $product_image=$_POST['img_url'];
-                            $product_quantity=$_POST['tittle'];
-
-                            $check_cart_numbers=mysqli_query($conn, "SELECT * FROM `cart_items`
-                                                            WHERE name = '$product_name' AND user_id = '$user_id'" )
-                                                            or die('query failed');
-                            
-                            if (mysqli_num_rows($check_cart_numbers)> 0) {
-                                $message[] = 'already added to cart';
-                            }
-                            mysqli_query($conn, "INSERT INTO `cart_items`(user_id, product_id, name, price, quantity, image)
-                                VALUES('$user_id', '$product_id',
-                                        '$product_name', '$product_price',
-                                        '$product_quantity', '$product_image')")
-                                or die('query failed');
-                            $message[] = 'product added to cart';
-                            }
-                            }
-                        ?>
+                        <form action="" method="POST" class="box">
+                            <input type="number" name="product_quantity" value="1" min="0" class="qty">
+                            <input type="hidden" name="product_id" value="<?= $row1['id'] ?>">
+                            <input type="hidden" name="product_name" value="<?= $row1['name'] ?>">
+                            <input type="hidden" name="product_price" value="<?= $row1['price'] ?>">
+                            <input type="hidden" name="product_image" value="<?= $row1['img_url'] ?>">
+                            <button type="submit" name="add_to_cart" class="btn btn-primary">Add to Cart</button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+    <?php
+    } else {
+        echo "No product found.";
+    }
+
+    // close the database connection
+    mysqli_close($conn);
+    ?>
+
+
+
     <?php
     $con = mysqli_connect("localhost", "root", "", "brief4");
 
@@ -94,7 +128,7 @@
         } else {
             $sql1 = "INSERT INTO comments (name, comment) VALUES ('$name', '$message')";
 
-            if ($con->query($sql1) === TRUE) {
+            if ($con->query($sql1) === true) {
                 echo "";
             } else {
                 echo "Error: " . $sql1 . "<br>" . $con->error;
@@ -134,10 +168,7 @@
         ?>
     </div>
 
-
-
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
